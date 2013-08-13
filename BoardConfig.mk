e893f117405424e21eb835964fb4f8fe92ed7b60
David Rodero <david@codiguiel.net>
2013-08-13 18:07:25 +0200

    latona: add support for TWRP recovery.
    Change-Id: I04d2da074b017e3dd46380837cf0498cf8781dc5
    Signed-off-by: David Rodero <david@codiguiel.net>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -54,6 +54,10 @@ BOARD_HAS_NO_SELECT_BUTTON := true
 TARGET_RECOVERY_PRE_COMMAND := "echo 1 > /cache/.startrecovery; sync;"
 BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/samsung/galaxysl/recovery/recovery_keys.c
 BOARD_USES_BML_OVER_MTD := true
+# TWRP support
+DEVICE_RESOLUTION := 480x800
+TW_INCLUDE_INJECTTWRP := true
+TW_NO_REBOOT_BOOTLOADER := true
 
 # fix this up by examining /proc/mtd on a running device
 BOARD_BOOTIMAGE_PARTITION_SIZE := 8388608
