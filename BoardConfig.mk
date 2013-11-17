136487b85b07b11e24d758c559a1c32d8b6b2992
David Rodero <david@codiguiel.net>
2013-11-17 14:34:56 +0100

    Reworked WiFi configuration (thanks to Nicholas Semendyaev!)
    Change-Id: I06f9406a20bc68e684ce5a6064b47e3458551546
    Signed-off-by: David Rodero <david@codiguiel.net>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -123,20 +123,20 @@ BOARD_FM_DEVICE := si4709
 # Hardware
 BOARD_HARDWARE_CLASS := device/samsung/galaxysl/cmhw
 
-# Connectivity - Wi-Fi
+# Wifi related defines
 USES_TI_MAC80211 := true
-ifdef USES_TI_MAC80211
-BOARD_WPA_SUPPLICANT_DRIVER := NL80211
-WPA_SUPPLICANT_VERSION := VER_0_8_X
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_wl12xx
-BOARD_HOSTAPD_DRIVER := NL80211
-BOARD_WLAN_DEVICE := wl12xx_mac80211
-BOARD_SOFTAP_DEVICE := wl12xx_mac80211
-WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/wl12xx_sdio.ko"
-WIFI_DRIVER_MODULE_NAME := "wl12xx_sdio"
-WIFI_FIRMWARE_LOADER := ""
 COMMON_GLOBAL_CFLAGS += -DUSES_TI_MAC80211
-endif
+WPA_SUPPLICANT_VERSION           := VER_0_8_X
+BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_wl12xx
+BOARD_HOSTAPD_DRIVER             := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_wl12xx
+PRODUCT_WIRELESS_TOOLS           := true
+BOARD_WLAN_DEVICE                := wl12xx_mac80211
+BOARD_SOFTAP_DEVICE              := wl12xx_mac80211
+WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/wl12xx_sdio.ko"
+WIFI_DRIVER_MODULE_NAME          := "wl12xx_sdio"
+WIFI_FIRMWARE_LOADER			 := ""
 
 TARGET_MODULES_SOURCE := "hardware/ti/wlan/mac80211/compat_wl12xx"
 
